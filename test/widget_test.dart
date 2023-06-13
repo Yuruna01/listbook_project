2746d8e60c65e2cb032a0e73c5085c1e7f2c4d7b
Yuruna01 <yusupss1007@gmail.com>
2023-06-13 22:41:56 +0700

    penyesuaian

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pricebook_project/main.dart';
+import 'package:listbook_project/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
