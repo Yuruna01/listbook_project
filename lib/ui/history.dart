4813e61d328370bc8118d0c675c9efad23b22183
Yuruna01 <yusupss1007@gmail.com>
2023-06-25 12:59:24 +0700

    Bug Overflowed

--- lib/ui/history.dart
+++ lib/ui/history.dart
@@ -23,72 +23,75 @@ class History extends StatelessWidget {
             floating: true,
             snap: true,
           ),
-          SliverFillRemaining(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Image.asset('assets/images/sale_4.png'),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(16),
-                    child: Text(
-                      'Aplikasi Listbook ini dikembangkan dengan tujuan penggunanya untuk mencari daftar laptop yang sangat lengkap dalam genggaman tangan dan mudah....',
-                      style: TextStyle(
-                        fontFamily: 'Indie Flower',
-                        fontSize: 18,
+          SliverPadding(
+            padding: EdgeInsets.all(16),
+            sliver: SliverToBoxAdapter(
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Image.asset('assets/images/sale_4.png'),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Aplikasi Listbook ini dikembangkan dengan tujuan penggunanya untuk mencari daftar laptop yang sangat lengkap dalam genggaman tangan dan mudah....',
+                        style: TextStyle(
+                          fontFamily: 'Indie Flower',
+                          fontSize: 18,
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 16),
-                    child: Text(
-                      'Partner Kerja Sama',
-                      style: TextStyle(
-                        fontFamily: 'Indie Flower',
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      child: Text(
+                        'Partner Kerja Sama',
+                        style: TextStyle(
+                          fontFamily: 'Indie Flower',
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  Container(
-                    height: 120,
-                    child: ListView.builder(
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 8, // Jumlah partner kerja sama
-                      itemBuilder: (BuildContext context, int index) {
-                        return Container(
-                          margin: EdgeInsets.symmetric(horizontal: 10),
-                          width: 100,
-                          height: 100,
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            image: DecorationImage(
-                              image: AssetImage(
-                                'assets/images/partner${index + 1}.jpeg',
+                    SizedBox(height: 10),
+                    Container(
+                      height: 120,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 8, // Jumlah partner kerja sama
+                        itemBuilder: (BuildContext context, int index) {
+                          return Container(
+                            margin: EdgeInsets.symmetric(horizontal: 10),
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              image: DecorationImage(
+                                image: AssetImage(
+                                  'assets/images/partner${index + 1}.jpeg',
+                                ),
+                                fit: BoxFit.cover,
                               ),
-                              fit: BoxFit.cover,
                             ),
-                          ),
-                        );
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
-                    child: Text(
-                      'Bergabunglah dengan kami dalam perjalanan untuk mencapai kesuksesan bersama, Kami bangga bermitra dengan perusahaan-perusahaan terkemuka dalam industri ini, Dengan kolaborasi yang kuat, kita dapat mencapai pertumbuhan dan kesuksesan yang berkelanjutan.',
-                      style: TextStyle(
-                        fontFamily: 'Indie Flower',
-                        fontSize: 18,
+                          );
+                        },
                       ),
                     ),
-                  ),
-                ],
-              ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+                      child: Text(
+                        'Bergabunglah dengan kami dalam perjalanan untuk mencapai kesuksesan bersama, Kami bangga bermitra dengan perusahaan-perusahaan terkemuka dalam industri ini, Dengan kolaborasi yang kuat, kita dapat mencapai pertumbuhan dan kesuksesan yang berkelanjutan.',
+                        style: TextStyle(
+                          fontFamily: 'Indie Flower',
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ),
           ),
         ],
